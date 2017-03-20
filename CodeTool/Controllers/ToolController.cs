02b22ad7b7c0337ebae4acd47c8eaff68c6ad6cc
Jason <522454279@qq.com>
2017-03-20 14:20:38 +0800

    md5编码

---
+++ CodeTool/Controllers/ToolController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using CodeTool.common;
+using JasonLib;
+using WebGrease.Css.Extensions;
+using CodeTool.Models.Index;
+using JasonLib.Web;
+using JasonLib.Web.Mvc;
+using SoufunLab.Framework.Web.Mvc;
+
+namespace CodeTool.Controllers
+{
+    public class ToolController : BaseController
+    {
+        //
+        // GET: /Default/
+
+        [ViewPage]
+        [Description("MD5编码")]
+        public ActionResult Md5()
+        {
+            return View();
+        }
+
+
+    }
+}
